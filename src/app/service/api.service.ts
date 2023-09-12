0362bdd967b8ae1e6566a4525c5b089070e29c83
Raul Olvera <raulolverat22@hotmail.com>
2023-09-12 16:46:08 -0700

    feat(app) installed bootstrap and services

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private urlApi = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  public getData(): Observable<any>{
+    const url = `${this.urlApi}/all`;
+    return this.http.get<any> (url);
+  }
+
+  public getByName(): Observable<any>{
+    const url = `${this.urlApi}/name/{name}`;
+    return this.http.get<any> (url);
+  }
+
+
+
+
+  
+}
