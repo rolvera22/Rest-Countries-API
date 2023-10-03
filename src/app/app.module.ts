817077ed1bc53f602244a1bad1f10f0ef433aed0
Raul Olvera <raulolverat22@hotmail.com>
2023-10-03 11:02:59 -0700

    style (app): added Filter by region

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { FilterComponent } from './filter/filter.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FilterComponent
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
